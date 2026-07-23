ca2fdf3317ae29d95effb2b3f55ab0fde106820f
javierjarart <javierjarart@users.noreply.github.com>
2026-07-22 23:53:10 -0400

    feat(gpu): WebGPU device setup and flow field texture upload

---
+++ js/gpu/compute.js
@@ -0,0 +1,2 @@
+export const WGSL_COMPUTE = '';
+// compute pipeline setup will go here
---
+++ js/gpu/render.js
@@ -0,0 +1,3 @@
+export const WGSL_RENDER_VERT = '';
+export const WGSL_RENDER_FRAG = '';
+// render pipeline setup will go here
